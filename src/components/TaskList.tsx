f5e94cf07548f5b7e1870598e89795d1f5f822cc
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-03-13 10:09:41 -0300

    criadas funcoes do desafio 01 do ignite

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,30 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === '') {
+      return;
+    }
+
+    setTasks([...tasks, {
+      id: Math.random() * 50,
+      title: newTaskTitle,
+      isComplete: false,
+    }])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const toggleTaskCompletion = tasks.find((task) => task.id === id);
+    if (toggleTaskCompletion) {
+      toggleTaskCompletion.isComplete = !toggleTaskCompletion.isComplete;
+      setTasks([...tasks.filter((task) => task.id !== id), toggleTaskCompletion])
+      return;
+    }
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks([...tasks.filter((task) => task.id !== id)])
   }
 
   return (
